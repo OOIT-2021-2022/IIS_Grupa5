0fa83a24ccd02adb5fb023930f3de5d48e5d8927
it11-2016 <it11-2016@AR4-ASISTENT.ftn.ns>
2022-03-15 10:36:56 +0100

    Druge vezbe - prvi deo

--- Grupa_5_2022/src/introduction/HelloWorld.java
+++ Grupa_5_2022/src/introduction/HelloWorld.java
@@ -6,6 +6,55 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, World!");
 
+		double firstNumber = 5;
+		double secondNumber = 2;
+
+		int result = (int) (firstNumber / secondNumber);
+		System.out.println(result);
+
+		System.out.println("5" + result);
+		System.out.println(Integer.parseInt("5") + result);
+		// System.out.println(Integer.parseInt("a")+result);
+
+		boolean alwaysTrue = true;
+		boolean alwaysFalse = false;
+		System.out.println(alwaysTrue && alwaysFalse);
+		System.out.println(alwaysTrue || alwaysFalse);
+		System.out.println(!alwaysTrue);
+		
+		// 1. Zadatak
+		System.out.println("Prvi zadatak");
+		int a = 24;
+		int b = 12;
+		String operation = "-";
+		if (operation == "+")
+			System.out.println(a + b);
+		else if (operation == "-")
+			System.out.println(a - b);
+		else if (operation == "/")
+			System.out.println(a / b);
+		else
+			System.out.println(a * b);
+
+		// 2. Zadatak
+		int rezultatFaktorijela = 1;
+
+		for (int i = 1; i < 6; i++) {
+			rezultatFaktorijela *= i;
+		}
+		System.out.println(rezultatFaktorijela);
+
+		// 3. Zadatak
+
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+				// break;
+				continue;
+			}
+			System.out.println(i);
+		}
+
 	}
 
 }
